adc2b5a78bd061ff85b68d796ee324c128d88cfc
isagkim <isagkim@gmail.com>
2024-12-10 07:50:11 +0000

    20241210 햄버거 만들기

--- algorithm/Lv1/Lv1_햄버거만들기(푸는중).py
+++ algorithm/Lv1/Lv1_햄버거만들기(푸는중).py
@@ -3,13 +3,27 @@ from urllib.parse import parse_qs
 
 def solution(ingredient):
     answer = 0
-    pattern = '1231'
-    target =  ''.join(map(str, ingredient))
-    for i in range(len(target)-len(pattern)+1):
-        if target[i:i+4] == pattern:
+    pattern = [1,2,3,1]
+    inLen = len(ingredient)
+    po = -1
+    while po < inLen-3:
+        po += 1
+        if po < 0:
+            po = 0
+        if ingredient[po:po+4] == pattern:
+            del ingredient[po:po+4]
             answer += 1
+            inLen -= 4
+            if len(ingredient) < 4:
+                break
+            if ingredient[po] == 1:
+                po -= 4
+            elif ingredient[po] == 2:
+                po -= 2
+            elif ingredient[po] == 3:
+                po -= 3
     return answer
-
 arr = [2, 1, 1, 2, 3, 1, 2, 3, 1]
+arr2 = [1, 3, 2, 1, 2, 1, 3, 1, 2]
 
-print(solution(arr))
+print(solution(arr2))
